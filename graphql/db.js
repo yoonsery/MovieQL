ebf23a7aba277dd7dc2fcd7245473c31dbffb910
yoonsery <yunsel2@gmail.com>
2021-07-23 15:11:48 +0900

    creating queries with arguments

--- graphql/db.js
+++ graphql/db.js
@@ -50,6 +50,6 @@ export const people = [
 ];
 
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => id === people.id);
+  const filteredPeople = people.filter((person) => String(id) === person.id);
   return filteredPeople[0];
 };
