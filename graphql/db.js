22e288a2a8a9b8b73500c239d3b7000ec17d467a
yoonsery <yunsel2@gmail.com>
2021-07-24 15:30:34 +0900

    example of wrapping a rest api with graphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,8 @@
-export let movies = [
-  {
-    id: 0,
-    name: 'Mad Max',
-    score: 10,
-  },
-  {
-    id: 1,
-    name: 'Black Widow',
-    score: 9,
-  },
-  {
-    id: 2,
-    name: 'Paddington',
-    score: 8,
-  },
-  {
-    id: 3,
-    name: 'Never I Have Ever',
-    score: 8,
-  },
-];
+import fetch from 'node-fetch';
 
-export const getMovies = () => movies;
+const API_URL = 'https://yts.mx/api/v2/list_movies.json';
 
-export const getByid = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id === id);
-  return filteredMovies[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
+export const getMovies = (limit, rating) =>
+  fetch(`${API_URL}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
